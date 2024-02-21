48992d6b229a175375ef81a96f8c9f8d313d8522
巧椰 菊地 <220065@jc-21.jp>
2024-02-21 10:05:28 +0900

    名前の保存をしようとしてる所まで

--- Server.cpp
+++ Server.cpp
@@ -2,10 +2,14 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <vector>
+#include<bitset>
 
 #pragma comment( lib, "ws2_32.lib" )
 
 using std::vector;
+using std::cout;
+using std::endl;
+using std::cin;
 
 // ポート番号
 const unsigned short SERVERPORT = 8888;
@@ -13,6 +17,18 @@ const unsigned short SERVERPORT = 8888;
 // 送受信するメッセージの最大値
 const unsigned int MESSAGELENGTH = 1024;
 
+//ありえない値
+const int INF = 9999999;
+
+struct PlayerInfo {
+	SOCKADDR_IN addr;
+	char playerName[MESSAGELENGTH];
+	int janken;
+
+	PlayerInfo() {
+		janken = INF;
+	}
+};
 
 int main()
 {
@@ -59,11 +75,11 @@ int main()
 
 	std::cout << "ok" << std::endl;
 
-	//60フレーム毎に一回全員に送信する情報を（ここではint）
-	int flame = 0;
 
 	//参加してるクライアントのアドレス情報
-	vector<SOCKADDR_IN> addrList_;
+	vector<PlayerInfo> playerList_;
+
+	vector<int> janken_;
 
 	int num = 0;
 
@@ -82,21 +98,15 @@ int main()
 			return 1;
 		}
 
-		
-
 		std::cout << "recv message = " << buff << std::endl;
 
-
-		/*for (int i = 0; i < addrList_.size(); i++) {
-
-			if (addrList_.at(i) == fromAddr) {
-
-			}
-		}*/
+		
 
 		//アドレス情報を保存する
-		addrList_.push_back(fromAddr);
-		std::cout << addrList_.size() << std::endl;
+		PlayerInfo info;
+		info.addr = fromAddr;
+		playerList_.push_back(info);
+		std::cout << playerList_.size() << std::endl;
 
 		//送信
 		ret = sendto(sock, buff, strlen(buff), 0, (struct sockaddr*)&fromAddr, fromlen);
@@ -108,15 +118,83 @@ int main()
 
 		std::cout << "Join OK " << buff << std::endl;
 
-		if (addrList_.size() == 4) {
+		if (playerList_.size() >= 3) {
 
 			break;
 		}
 
 	}
 
+	//ここのwhileはプレイヤーリストのサイズ分だけ
+	int pNum = 0;
+	while (playerList_.size() < pNum) {
 
-	//ここで情報を常に受け取り、一定時間毎にすべてのクライアントに情報を返す
+
+		//送信
+		char buff[MESSAGELENGTH] = playerList_.at(pNum).playerName + "さんのじゃんけんの手を入力してください";
+		//SOCKADDR_IN fromAddr;
+		int fromlen = sizeof(SOCKADDR_IN);//ここのsizeofに変数のほうを入れてる理由が何かあるのか
+		ret = sendto(sock, buff, strlen(buff), 0, (struct sockaddr*)&playerList_.at(pNum), fromlen);
+		if (ret != strlen(buff))
+		{
+			std::cout << "sendtoError" << WSAGetLastError() << std::endl;
+			return 1;
+		}
+
+		//受信
+		char buff[MESSAGELENGTH];
+		SOCKADDR_IN fromAddr;
+		int fromlen = sizeof(fromAddr);
+		ret = recvfrom(sock, buff, sizeof(buff), 0, (SOCKADDR*)&fromAddr, &fromlen);
+		if (ret == SOCKET_ERROR)
+		{
+			std::cout << "recvtoError" << WSAGetLastError() << std::endl;
+			return 1;
+		}
+		std::cout << "recv message = " << buff << std::endl;
+
+		if (buff == "ぐー") {
+			janken_.emplace_back(9);
+		}
+		else if (buff == "ちょき") {
+			janken_.emplace_back(2);
+		}
+		else if (buff == "ぱー") {
+			janken_.emplace_back(4);
+		}
+
+	}
+
+	std::string ref_ = { "負け","勝ち" };
+
+	int tmp = 0;
+	int size = janken_.size();
+
+	for (int i = 0; i < size; i++) {
+		tmp = tmp | janken_.at(i);
+	}
+
+	int sTmp = tmp >> 1;//算術シフト
+
+	int result = sTmp & tmp;
+
+	if (result == 7 || result == 0) {
+		cout << "引き分け！" << endl;
+	}
+	else {
+		for (int i = 0; i < size; i++) {
+			tmp = janken_.at(i);
+			std::bitset<4> bit(result & tmp);
+			int countBit = bit.count();
+			cout << janken_.at(i).getName() << "さんは" << ref_[countBit] << endl;
+			if (countBit == 1) {
+				janken_.at(i).CountW();
+			}
+		}
+	}
+
+
+	//ここで勝敗を返す
 	while (true)
 	{
 		//受信
@@ -131,27 +209,21 @@ int main()
 		}
 
 		std::cout << "recv message = " << buff << std::endl;
-		
 
-		//1秒ごとに送信
-		if (flame <= 60) {
+		for (int i = 0; i < addrList_.size(); i++) {
 
-			for (int i = 0; i < addrList_.size(); i++) {
-			
-				ret = sendto(sock, buff, strlen(buff), 0, (struct sockaddr*)&addrList_.at(i), fromlen);
-				if (ret != strlen(buff))
-				{
-					std::cout << "sendtoError" << WSAGetLastError() << std::endl;
-					return 1;
-				}
-
-				std::cout << "sended message " << buff << std::endl;
+			ret = sendto(sock, buff, strlen(buff), 0, (struct sockaddr*)&addrList_.at(i), fromlen);
+			if (ret != strlen(buff))
+			{
+				std::cout << "sendtoError" << WSAGetLastError() << std::endl;
+				return 1;
 			}
-		}
-		
 
+			std::cout << "sended message " << buff << std::endl;
+		}
 	}
 
+	
 
 	// ここまでこないけど、ソケット破棄
 	ret = closesocket(sock);
